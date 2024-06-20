d743d10bed3151f949878b37671e208f7b3061db
JulianRuano <julianrmajin@gmail.com>
2024-06-20 12:27:27 -0500

    refactor: Remove unused code for Department and City models

--- product/src/main/java/com/product/infraestructure/adapters/output/jpaAdapter/repository/IDepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.product.infraestructure.adapters.output.jpaAdapter.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.product.infraestructure.adapters.output.jpaAdapter.entity.DepartmentEntity;
-
-public interface IDepartmentRepository extends JpaRepository<DepartmentEntity, Long>{
-    
-}
